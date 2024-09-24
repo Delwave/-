e8fe26c0cbb5646fd02a9b758e9e4c7b68c93589
Delwave <xomaqeu@yandex.ru>
2024-09-24 09:21:42 +0300

    Add files via upload

---
+++ passgame.py
@@ -0,0 +1,8 @@
+def get_password(n):
+    result = ''
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
+                result += f'{i}{j}'
+    return result
+            
